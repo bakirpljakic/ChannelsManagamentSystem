433637bffa20e4bdfc0f283fbf393a121b95f87c
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-04-28 21:06:16 +0200

    Added Navbar

---
+++ channels/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const [selectedRegion, setSelectedRegion] = useState(''); // Stanje za odabranu regiju
+
+  const handleRegionChange = (event) => {
+    const selectedRegion = event.target.value;
+    setSelectedRegion(selectedRegion);
+    // Ovdje možete implementirati logiku za odabir regije (npr. navigacija ili promjena stanja)
+    console.log(`Odabrana regija: ${selectedRegion}`);
+  };
+
+  const regions = [
+    'Sarajevo',
+    'Mostar',
+    'Banja Luka',
+    'Tuzla',
+    'Zenica',
+    'Bihać',
+    'Doboj',
+    'Trebinje',
+    // Dodajte ostale regije po potrebi
+  ];
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-center">
+        <Link to="/tv" className="navbar-item">TV</Link>
+        <Link to="/radio" className="navbar-item">Radio</Link>
+        <Link to="/billboard" className="navbar-item">Billboard</Link>
+        <Link to="/website" className="navbar-item">Website</Link>
+        <Link to="/display" className="navbar-item">Display</Link>
+      </div>
+
+      {/* Dropdown za odabir regije */}
+      <div className="dropdown-container">
+        <select
+          className="input-select"
+          value={selectedRegion}
+          onChange={handleRegionChange}
+        >
+          <option value="">All regions</option>
+          {regions.map((region, index) => (
+            <option key={index} value={region}>{region}</option>
+          ))}
+        </select>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
