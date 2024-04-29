78ca6d2aca5bd8a9edd5d0079f1c8df175e2b05a
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-04-30 00:55:36 +0200

    added Routes

--- channels/src/App.jsx
+++ channels/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Details from '../components/Details/Details';
 import TV from '../components/TV/TV';
 
 
 function App() {
-
   return (
-    <div>
-      <Router>
+    <Router>
+      <div className="App">
         <Navbar />
-        <Route path="/tv" element={<TV />} />
-        <Route path="/radio" element={<TV />} />
-        <Route path="/billboard" element={<TV />} />
-        <Route path="/website" element={<TV />} />
-        <Route path="/display" element={<TV />} />
-        <Route path="/details" element={<Details />} />
-      </Router>
-    </div>
-
-  )
+        <Routes>
+          <Route path="/details" element={<Details />} />
+          <Route path="/tv" element={<TV />} />
+          <Route path="/radio" element={<TV />} />
+          <Route path="/billboard" element={<TV />} />
+          <Route path="/website" element={<TV />} />
+          <Route path="/display" element={<TV />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
