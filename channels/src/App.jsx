64a0e6fc04c75eb1a16d3f0894cd85e630391e4d
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-04-30 00:41:33 +0200

    small changes

--- channels/src/App.jsx
+++ channels/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import Navbar from '../components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
-import TV from '../components/TV/TV';
 import Details from '../components/Details/Details';
+import TV from '../components/TV/TV';
+
 
 function App() {
 
